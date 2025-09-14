3b045ad1cd0618a45e45a04967c1d491cc56a224
josh <github@junk.josh.com>
2025-09-14 15:56:09 -0400

    Create sample-run.bat

---
+++ sample-run.bat
@@ -0,0 +1,1 @@
+text_to_gds.py --font .\font4x6.txt --text .\pi-billion-p-grid.txt --out .\pi-billion-p-grid-8run.gds --pixel-size 750 --unit 1e-9 --precision 1e-9 --progress-every 10000 --prebuilt-digits-len 8
